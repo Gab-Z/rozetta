73ddb3a772571ee7141357697c6904f3fc51bc36
Gab-Z <glevassor@gmail.com>
2017-12-27 16:07:14 +0100

    commit

--- maskMaker/js/main.js
+++ maskMaker/js/main.js
@@ -3,14 +3,93 @@ var points,
     triangles,
     triangleSelectionIndex = 0;
 window.onload = function(){
-  testIntersection()
-//  init( positions, indices );
+  //testIntersection()
+  init( positions, indices );
 };
 var init = function( _positions, _indices){
   points = positionsToPoints( _positions );
   triangles = indicesToTriangles( _indices, points );
-  selectFirstTriangle.start();
+//  selectFirstTriangle.start();
+drawTriangle( 0 )
 };
+function drawTriangle( i ){
+  var canvas = document.body.appendChild( document.createElement( "canvas" ) ),
+      ctx = canvas.getContext( "2d" );
+  canvas.id = "canvas";
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+  var triangle = triangles.triangles[ i ],
+      neighbours = triangle.neighbours,
+      intersec = intersection(0, 0, triangle.CtoA, 0, triangle.AtoB, triangle.BtoC),
+      pts2d = {
+        a:{x:0,y:0},
+        b:{x:0,y:triangle.AtoB},
+        c:{x:intersec[0],y:intersec[2] }
+      };
+  ctx.restore();
+  ctx.save();
+  ctx.clearRect( 0,0, canvas.width, canvas.height );
+  ctx.translate( canvas.width / 2, canvas.height / 2 );
+  ctx.beginPath();
+  ctx.moveTo( pts2d.a.x, pts2d.a.y );
+  ctx.lineTo( pts2d.b.x, pts2d.b.y );
+  ctx.lineTo( pts2d.c.x, pts2d.c.y );
+  ctx.closePath();
+  ctx.stroke();
+  triangle.pts2d = pts2d;
+  for( var k in neighbours ){
+    var mainEdgeName = k,
+        neighbour = triangles.triangles[ neighbours[ k ].id ],
+        matchs = neighbours[ k ].matchs;
+    for( var n in matchs ){
+      neighbour.pts2d[ matchs[ n ] ] = triangle.pts2d[ n ]
+    }
+    var thirdPt;
+    if(k=="ab"){
+      thirdPt = triangle.pts2d.c
+    }else if(k=="bc"){
+      thirdPt = triangle.pts2d.a
+    }else if(k=="ca"){
+      thirdPt = triangle.pts2d.b
+    }
+    var n2d = neighbour.pts2d;
+    if( !n2d.a){
+      n2d.a = findNeighbour(n2d.c,n2d.b,thirdPt,neighbour.CtoA, neighbour.AtoB)
+    }else if( !n2d.b){
+      n2d.b  = findNeighbour(n2d.a,n2d.c,thirdPt,neighbour.AtoB, neighbour.BtoC)
+    }else if( !n2d.c){
+      n2d.c  = findNeighbour(n2d.a,n2d.b,thirdPt,neighbour.CtoA, neighbour.BtoC)
+    }
+    if(n2d.a && n2d.b && n2d.c){
+      ctx.beginPath();
+      ctx.moveTo( n2d.a.x, n2d.a.y );
+      ctx.lineTo( n2d.b.x, n2d.b.y );
+      ctx.lineTo( n2d.c.x, n2d.c.y );
+      ctx.closePath();
+      ctx.stroke();
+    }
+  }
+}
+function findNeighbour(edgeA,edgeB,mainC,sizeA, sizeB){
+  var ax = edgeA.x,
+      ay = edgeA.y,
+      bx = edgeB.x,
+      by = edgeB.y,
+      cx = mainC.x,
+      cy = mainC.y,
+      intersec = intersection(ax, ay, sizeA, bx, by, sizeB),
+      nx = intersec[ 0 ],
+      ny = intersec[ 2 ];
+  if(    is_in_triangle (nx,ny,ax,ay,bx,by,cx,cy)
+      || is_in_triangle (cx,cy,ax,ay,bx,by,nx,ny)
+      || testIntersect(ax, ay, nx, ny, bx, by, cx, cy)
+      || testIntersect(bx, by, nx, ny, ax, ay, cx, cy)
+  ){
+    return {x:intersec[ 1 ],y:intersec[ 3 ]}
+  }else{
+    return  {x:intersec[ 0 ],y:intersec[ 2 ]}
+  }
+}
 var selectFirstTriangle = {
   start: function(){
     toolBox.navSelect();
@@ -351,7 +430,7 @@ function testIntersection(){
   var canvas = document.body.appendChild( document.createElement( "canvas" ) ),
       ctx = canvas.getContext( "2d" ),
       p1 = {x:0,y:0},
-      p2 = {x:150,y:200},
+      p2 = {x:-150,y:-200},
       l1 = 150,
       l2 = 180,
       intersec = intersection(p1.x, p1.y, l1, p2.x, p2.y, l2),
@@ -457,6 +536,75 @@ function intersection(x0, y0, r0, x1, y1, r1) {
 
     	return (crossProduct < 0 ? -1 : 1) * Math.acos((b * b + c * c  - aSq) / (2 * b * c)); // cosine rule
     }
+function is_in_triangle (px,py,ax,ay,bx,by,cx,cy){
+
+  //credit: http://www.blackpawn.com/texts/pointinpoly/default.html
+
+  var v0 = [cx-ax,cy-ay];
+  var v1 = [bx-ax,by-ay];
+  var v2 = [px-ax,py-ay];
+
+  var dot00 = (v0[0]*v0[0]) + (v0[1]*v0[1]);
+  var dot01 = (v0[0]*v1[0]) + (v0[1]*v1[1]);
+  var dot02 = (v0[0]*v2[0]) + (v0[1]*v2[1]);
+  var dot11 = (v1[0]*v1[0]) + (v1[1]*v1[1]);
+  var dot12 = (v1[0]*v2[0]) + (v1[1]*v2[1]);
+
+  var invDenom = 1/ (dot00 * dot11 - dot01 * dot01);
+
+  var u = (dot11 * dot02 - dot01 * dot12) * invDenom;
+  var v = (dot00 * dot12 - dot01 * dot02) * invDenom;
+
+  return ((u >= 0) && (v >= 0) && (u + v < 1));
+}
+function testIntersect(x1, y1, x2, y2, x3, y3, x4, y4){
+	var a1, a2, b1, b2, c1, c2;
+	var r1, r2 , r3, r4;
+	var denom, offset, num;
+
+	// Compute a1, b1, c1, where line joining points 1 and 2
+	// is "a1 x + b1 y + c1 = 0".
+	a1 = y2 - y1;
+	b1 = x1 - x2;
+	c1 = (x2 * y1) - (x1 * y2);
+
+	// Compute r3 and r4.
+	r3 = ((a1 * x3) + (b1 * y3) + c1);
+	r4 = ((a1 * x4) + (b1 * y4) + c1);
+
+	// Check signs of r3 and r4. If both point 3 and point 4 lie on
+	// same side of line 1, the line segments do not intersect.
+	if ((r3 !== 0) && (r4 !== 0) && sameSign(r3, r4)){
+		return 0; //return that they do not intersect
+	}
+
+	// Compute a2, b2, c2
+	a2 = y4 - y3;
+	b2 = x3 - x4;
+	c2 = (x4 * y3) - (x3 * y4);
+
+	// Compute r1 and r2
+	r1 = (a2 * x1) + (b2 * y1) + c2;
+	r2 = (a2 * x2) + (b2 * y2) + c2;
+
+	// Check signs of r1 and r2. If both point 1 and point 2 lie
+	// on same side of second line segment, the line segments do
+	// not intersect.
+	if ((r1 !== 0) && (r2 !== 0) && (sameSign(r1, r2))){
+		return 0; //return that they do not intersect
+	}
+
+	//Line segments intersect: compute intersection point.
+	denom = (a1 * b2) - (a2 * b1);
+
+	if (denom === 0) {
+		return 1; //collinear
+	}
+
+	// lines_intersect
+	return 1; //lines intersect, return true
+}
+const sameSign = (a, b) => (a * b) > 0;
 var compareVec3 = function(pta,ptb){
   if( pta.x == ptb.x && pta.y == ptb.y && pta.z == ptb.z ) return true;
   return false;
