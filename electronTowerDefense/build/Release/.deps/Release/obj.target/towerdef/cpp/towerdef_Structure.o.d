035f1d9598b8f6faef4de3a5cc79d2d91b41d98d
Gab-Z <glevasor@gmail.com>
2018-04-02 19:43:48 +0200

    commit

--- electronTowerDefense/build/Release/.deps/Release/obj.target/towerdef/cpp/towerdef_Structure.o.d
+++
@@ -1,5 +0,0 @@
-cmd_Release/obj.target/towerdef/cpp/towerdef_Structure.o := g++ '-DNODE_GYP_MODULE_NAME=towerdef' '-DUSING_UV_SHARED=1' '-DUSING_V8_SHARED=1' '-DV8_DEPRECATION_WARNINGS=1' '-D_LARGEFILE_SOURCE' '-D_FILE_OFFSET_BITS=64' '-DBUILDING_NODE_EXTENSION' -I/home/afev/.electron-gyp/.node-gyp/iojs-1.8.2/include/node -I/home/afev/.electron-gyp/.node-gyp/iojs-1.8.2/src -I/home/afev/.electron-gyp/.node-gyp/iojs-1.8.2/deps/uv/include -I/home/afev/.electron-gyp/.node-gyp/iojs-1.8.2/deps/v8/include -I../node_modules/nan  -fPIC -pthread -Wall -Wextra -Wno-unused-parameter -m64 -O3 -fno-omit-frame-pointer -fno-rtti -fno-exceptions -std=gnu++0x -MMD -MF ./Release/.deps/Release/obj.target/towerdef/cpp/towerdef_Structure.o.d.raw   -c -o Release/obj.target/towerdef/cpp/towerdef_Structure.o ../cpp/towerdef_Structure.cpp
-Release/obj.target/towerdef/cpp/towerdef_Structure.o: \
- ../cpp/towerdef_Structure.cpp ../cpp/towerdef_Structure.h
-../cpp/towerdef_Structure.cpp:
-../cpp/towerdef_Structure.h:
--- electronTowerDefense/build/Release/.deps/Release/towerdef.node.d
+++
@@ -1,1 +0,0 @@
-cmd_Release/towerdef.node := rm -rf "Release/towerdef.node" && cp -af "Release/obj.target/towerdef.node" "Release/towerdef.node"
